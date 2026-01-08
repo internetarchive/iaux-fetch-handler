cc704c26e0df2826300400cb6ca0730f98580b66
Jason Buckner <jason@jasonbuckner.com>
2026-01-08 12:56:40 -0800

    Add exports

--- index.ts
+++ index.ts
@@ -4,3 +4,7 @@ export {
   FetchRetrier,
   FetchRetrierInterface,
 } from './src/fetch-retry/fetch-retrier';
+export { DefaultRetryConfiguration } from './src/fetch-retry/configuration/default-retry-configuration';
+export { NoRetryConfiguration } from './src/fetch-retry/configuration/no-retry-configuration';
+export type { RetryConfiguring } from './src/fetch-retry/configuration/retry-configuring';
+export type { FetchOptions } from './src/fetch-options';
